ab4b816ffe86e71ce589b6a72086f1196f352e6b
landonb <landonb@users.noreply.github.com>
2020-02-08 01:53:23 -0600

    DX: Update comments.

--- readme-using.make.sh
+++ readme-using.make.sh
@@ -21,9 +21,10 @@ cat bundle_/dubs_web_hatch/README.rst >> README-USING.rst
 
 # Forked repos (not their original):
 #  cat bundle_/ansible-vim/README.md >> README-USING.rst
-#  cat bundle_/vim-jsx/README.md >> README-USING.rst
+#  # DEPRECATED: cat bundle_/vim-jsx/README.md >> README-USING.rst
 #  cat bundle_/vim-markdown/README.md >> README-USING.rst
 #  cat bundle_/vim-surround/README.markdown >> README-USING.rst
+#  cat bundle_/vim-tmux-navigator/README.markdown >> README-USING.rst
 
 cat readme-using.pt2.rst >> README-USING.rst
 
