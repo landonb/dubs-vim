fe043fa3d5a4b4f543cd106ef4030dcfca6e80d2
Landon Bouma <landonb@users.noreply.github.com>
2024-12-10 19:46:44 -0600

    Improve: Add Markdown READMEs to generated help
    - Using `pandoc` md → rst conversion.

--- doc/readme-using.make.sh
+++ doc/readme-using.make.sh
@@ -10,6 +10,8 @@ build_readme_using () {
   local startd="${HOME}/.vim/pack/landonb/start"
   # local optd="${HOME}/.vim/pack/landonb/optd"
 
+  local depoxy="${HOME}/.vim/pack/DepoXy/start"
+
   local embrace="${HOME}/.vim/pack/embrace-vim/start"
 
   # PARITY: See directories under ~/.vim/pack/landonb/start
@@ -23,54 +25,74 @@ build_readme_using () {
   #           do nothing, because this task is not a priority,
   #           and I need to focus my attention elsewhere.)
 
-  # (forked; not reST) cat ${startd}/ansible-vim/README.md >> ${target}
-  cat ${startd}/dubs_after_dark/README.rst >> ${target}
-  cat ${startd}/dubs_appearance/README.rst >> ${target}
-  cat ${startd}/dubs_buffer_fun/README.rst >> ${target}
-  cat ${startd}/dubs_edit_juice/README.rst >> ${target}
-  # ISOFF/2024-12-05: The landonb/dubs_file_finder plugin
-  # is now deprecated.
-  #  cat ${HOME}/.vim/pack/landonb/opt/dubs_file_finder/README.rst >> ${target}
-  cat ${startd}/dubs_ftype_mess/README.rst >> ${target}
-  cat ${startd}/dubs_grep_steady/README.rst >> ${target}
-  cat ${startd}/dubs_html_entities/README.rst >> ${target}
-  cat ${startd}/dubs_mescaline/README.rst >> ${target}
-  cat ${startd}/dubs_project_tray/README.rst >> ${target}
-  cat ${startd}/dubs_quickfix_wrap/README.rst >> ${target}
-  cat ${startd}/dubs_style_guard/README.rst >> ${target}
-  # cat ${optd}/dubs_syntastic_wrap/README.rst >> ${target}
-  cat ${startd}/dubs_toggle_textwrap/README.rst >> ${target}
-  # (forked; not reST) cat ${startd}/QFEnter/README.md >> ${target}
-  # (not reST) cat ${startd}/vim-buffer-ring/README.md >> ${target}
-  # (forked) cat ${startd}/vim-classic-taglist/README.rst >> ${target}
-  # (not reST) cat ${startd}/vim-command-line-clock/README.md >> ${target}
-  cat ${startd}/vim-fullscreen-toggle/README.rst >> ${target}
-  cat ${embrace}/vim-goto-file-sh/README.rst >> ${target}
-  # (not reST) cat ${startd}/vim-lcd-project-root/README.md >> ${target}
-  # (forked; not reST) cat ${startd}/vim-markdown/README.md >> ${target}
-  cat ${startd}/vim-mkspell-when-stale/README.rst >> ${target}
-  # (not reST) cat ${startd}/vim-netrw-cfg-split-explorer/README.md >> ${target}
-  # (not reST) cat ${startd}/vim-netrw-link-resolve/README.md >> ${target}
-  cat ${startd}/vim-nicer-file-changed-prompt/README.rst >> ${target}
-  cat ${startd}/vim-ovm-easyescape-kj-jk/README.rst >> ${target}
-  cat ${startd}/vim-ovm-seven-of-spines/README.rst >> ${target}
-  cat ${startd}/vim-reSTfold/README.rst >> ${target}
-  cat ${startd}/vim-reST-highdefs/README.rst >> ${target}
-  cat ${startd}/vim-reST-highfive/README.rst >> ${target}
-  cat ${startd}/vim-reST-highline/README.rst >> ${target}
-  cat ${startd}/vim-select-mode-stopped-down/README.rst >> ${target}
-  cat ${embrace}/vim-web-hatch/README.rst >> ${target}
-  # (not reST) cat ${startd}/vim-source-reloader/README.md >> ${target}
-  # (forked; not reST) cat ${startd}/vim-surround/README.markdown >> ${target}
-  # (not reST) cat ${startd}/vim-title-bar-time-of-day/README.md >> ${target}
-  # (forked; not reST) cat ${startd}/vim-tmux-navigator/README.markdown >> ${target}
-  # (forked; not reST) cat ${startd}/vim-unimpaired/README.markdown >> ${target}
+  # (forked) md2rst "${startd}/ansible-vim/README.md" >> ${target}
+  cat "${startd}/dubs_after_dark/README.rst" >> ${target}
+  cat "${startd}/dubs_appearance/README.rst" >> ${target}
+  cat "${startd}/dubs_buffer_fun/README.rst" >> ${target}
+  cat "${startd}/dubs_edit_juice/README.rst" >> ${target}
+  # (opted) cat "${optd}/opt/dubs_file_finder/README.rst" >> ${target}
+  cat "${startd}/dubs_ftype_mess/README.rst" >> ${target}
+  cat "${startd}/dubs_grep_steady/README.rst" >> ${target}
+  cat "${startd}/dubs_html_entities/README.rst" >> ${target}
+  cat "${startd}/dubs_mescaline/README.rst" >> ${target}
+  cat "${startd}/dubs_project_tray/README.rst" >> ${target}
+  cat "${startd}/dubs_quickfix_wrap/README.rst" >> ${target}
+  cat "${startd}/dubs_style_guard/README.rst" >> ${target}
+  # (opted) cat "${optd}/dubs_syntastic_wrap/README.rst" >> ${target}
+  cat "${startd}/dubs_toggle_textwrap/README.rst" >> ${target}
+  # (forked) cat ${startd}/QFEnter/README.md" >> ${target}
+  md2rst "${embrace}/vim-async-mapper/README.md" >> ${target}
+  md2rst "${startd}/vim-buffer-ring/README.md" >> ${target}
+  # (forked) md2rst "${startd}/vim-classic-taglist/README.rst" >> ${target}
+  md2rst "${depoxy}/vim-clip-expand-path/README.md" >> ${target}
+  md2rst "${startd}/vim-command-line-clock/README.md" >> ${target}
+  md2rst "${depoxy}/vim-depoxy-coc-defaults/README.md" >> ${target}
+  cat "${startd}/vim-fullscreen-toggle/README.rst" >> ${target}
+  cat "${embrace}/vim-goto-file-sh/README.rst" >> ${target}
+  md2rst "${startd}/vim-lcd-project-root/README.md" >> ${target}
+  # (forked) md2rst "${startd}/vim-markdown/README.md" >> ${target}
+  cat "${startd}/vim-mkspell-when-stale/README.rst" >> ${target}
+  md2rst "${startd}/vim-netrw-cfg-split-explorer/README.md" >> ${target}
+  md2rst "${startd}/vim-netrw-link-resolve/README.md" >> ${target}
+  cat "${startd}/vim-nicer-file-changed-prompt/README.rst" >> ${target}
+  cat "${startd}/vim-ovm-easyescape-kj-jk/README.rst" >> ${target}
+  cat "${startd}/vim-ovm-seven-of-spines/README.rst" >> ${target}
+  cat "${startd}/vim-reSTfold/README.rst" >> ${target}
+  cat "${startd}/vim-reST-highdefs/README.rst" >> ${target}
+  cat "${startd}/vim-reST-highfive/README.rst" >> ${target}
+  cat "${startd}/vim-reST-highline/README.rst" >> ${target}
+  cat "${startd}/vim-select-mode-stopped-down/README.rst" >> ${target}
+  cat "${embrace}/vim-web-hatch/README.rst" >> ${target}
+  md2rst "${startd}/vim-source-reloader/README.md" >> ${target}
+  # (forked) md2rst "${startd}/vim-surround/README.markdown" >> ${target}
+  md2rst "${startd}/vim-title-bar-time-of-day/README.md" >> ${target}
+  # (forked) md2rst "${startd}/vim-tmux-navigator/README.markdown" >> ${target}
+  # (forked) md2rst "${startd}/vim-unimpaired/README.markdown" >> ${target}
 
-  # local dxy_start="${HOME}/.vim/pack/DepoXy/start"
-  # (not reST) cat ${dxy_start}/vim-clip-expand-path/README.md >> ${target}
-  # (not reST) cat ${dxy_start}/vim-depoxy-coc-defaults/README.md >> ${target}
+  cat "readme-using.pt2.rst" >> ${target}
+}
 
-  cat readme-using.pt2.rst >> ${target}
+# ***
+
+# REFER: pandoc `--from` and `--to` are not required if the
+# file extensions also match. E.g.,
+#   # These are the same
+#   $ pandoc --output=index.rst index.md
+#   $ pandoc --from=markdown --to=rst --output=index.rst index.md
+
+# SAVVY: pandoc doesn't convert Markdown [`links`][links] well
+# - It creates ``links` <https://links>`__ which doesn't
+#   render as a link, but renders verbatim.
+# - So we'll convert [`links`][links] to [links][links] first.
+
+md2rst () {
+  local file="$1"
+
+  sanitize_links () {
+    sed -E 's/\[`([^`]*)`\]/\[\1\]/g' "${file}"
+  }
+
+  pandoc --from=markdown --to=rst <(sanitize_links)
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
